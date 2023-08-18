125265e4c4e41622d211524715c5fa8cdec3b3af
Suraj Singhal <117528760+MrStark1234@users.noreply.github.com>
2023-08-18 23:11:09 +0530

    18-8 code commited

--- JavaScript-practice/index.js
+++ JavaScript-practice/index.js
@@ -11,10 +11,51 @@ function primeNumber(n){
 }
 
 // console.log(primeNumber(11))
+
+//---------------------------------------------------------------------------------------------------------
+
  let sum = 0
 let a = [1,2,3,4,5]
 a.forEach((item)=>{
  return(sum += item)
     }
 )
-console.log(sum)
+// console.log(sum)
+
+
+
+//Q Write a javascript function to check whether an 'input' is an array or not?
+
+// function isArray(arr){
+//    if(Array.isArray(arr)){
+//     return true
+//    }else{
+//     return false
+//    }
+// }
+// console.log(isArray(1))
+
+
+// function isArray(arr) {
+//   if (Object.prototype.toString.call(arr) === '[object Array]') {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+
+// console.log(isArray(1)); // Output: true
+// // console.log(isArray("not an array")); // Output: false
+
+
+
+function isArray(arr){
+   if(arr instanceof Array){
+    return true
+   }else{
+    return false
+   }
+}
+console.log(isArray(1))
+
+
